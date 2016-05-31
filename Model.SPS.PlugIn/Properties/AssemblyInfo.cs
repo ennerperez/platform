56edab648f78ec727bfe7d7c3298f01f417fe9dd
Enner Pérez <ennerperez@gmail.com>
2016-05-31 03:05:35 -0400

    Models + Data

--- Model.SPS.PlugIn/Properties/AssemblyInfo.cs
+++ Model.SPS.PlugIn/Properties/AssemblyInfo.cs
@@ -3,7 +3,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Model SPS Plugin Portable")]
-[assembly: AssemblyDescription("Platform Model SPS Plugin Pattern (Portable)")]
-
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Model SPS Plugin")]
+[assembly: AssemblyDescription("Platform Model SPS Plugin Pattern")]
