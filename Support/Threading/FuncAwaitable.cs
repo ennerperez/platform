55b0e25ff77557c9b742ec9bc9f8dcb88351863a
Enner Pérez <ennerperez@gmail.com>
2016-06-19 23:46:05 -0400

    IAwaitable Preview

---
+++ Support/Threading/FuncAwaitable.cs
@@ -0,0 +1,32 @@
+﻿#if NETFX_45
+
+using Platform.Support.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Platform.Support.Extensions
+{
+
+    internal struct FuncAwaitable<TResult> : IAwaitable<TResult>
+    {
+        private readonly Func<TResult> function;
+
+        public FuncAwaitable(Func<TResult> function)
+        {
+            this.function = function;
+        }
+
+        public IAwaiter<TResult> GetAwaiter()
+        {
+            return new FuncAwaiter<TResult>(this.function);
+        }
+    }
+        
+}
+
+#endif
---
+++ Support/Threading/FuncAwaiter.cs
@@ -0,0 +1,42 @@
+﻿#if NETFX_45
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Support.Threading
+{
+    public struct FuncAwaiter<TResult> : IAwaiter<TResult>
+    {
+        private readonly Task<TResult> task;
+
+        public FuncAwaiter(Func<TResult> function)
+        {
+            this.task = new Task<TResult>(function);
+            this.task.Start();
+        }
+
+        bool IAwaiter<TResult>.IsCompleted
+        {
+            get
+            {
+                return this.task.IsCompleted;
+            }
+        }
+
+        TResult IAwaiter<TResult>.GetResult()
+        {
+            return this.task.Result;
+        }
+
+        void OnCompleted(Action continuation)
+        {
+            new Task(continuation).Start();
+        }
+    }
+}
+
+#endif
