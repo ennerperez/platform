f82e7f5725a8808874e39ab5b9a3d4803bab40d7
Enner Pérez <ennerperez@gmail.com>
2016-06-24 00:36:58 -0400

    NuGet Generation

--- Support.Net/Properties/AssemblyInfo.cs
+++ Support.Net/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Support Net")]
-[assembly: AssemblyDescription("Platform Support for Net")]
+[assembly: AssemblyDescription("Platform Support for Networking")]
 
 [assembly: Guid("f8245d33-07f5-4b29-80e0-99ad7cc9bcb2")]
