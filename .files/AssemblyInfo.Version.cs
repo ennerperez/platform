f3bbfa9834a099700b735b04e021be125c6b1ce7
Enner Pérez <ennerperez@gmail.com>
2016-08-18 20:52:04 -0400

    Preview

--- .files/AssemblyInfo.Version.cs
+++ .files/AssemblyInfo.Version.cs
@@ -7,6 +7,6 @@ using Platform.Support;
 using Platform.Support.Attributes;
 #endif
 
-[assembly: AssemblyVersion("3.2.11.10")]
+[assembly: AssemblyVersion("3.2.11.11")]
 [assembly: AssemblyFileVersion("3.2")]
 
