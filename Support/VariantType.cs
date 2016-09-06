529b37f3c5f4005701beecf13b105f6ca6879639
Enner Pérez <ennerperez@gmail.com>
2016-09-06 02:12:04 -0400

    VariantType Enum

---
+++ Support/VariantType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platform.Support
+{
+    public enum VariantType
+    {
+        Empty = 0,
+        Null = 1,
+        Short = 2,
+        Integer = 3,
+        Single = 4,
+        Double = 5,
+        Currency = 6,
+        Date = 7,
+        String = 8,
+        Object = 9,
+        Error = 10,
+        Boolean = 11,
+        Variant = 12,
+        DataObject = 13,
+        Decimal = 14,
+        Byte = 17,
+        Char = 18,
+        Long = 20,
+        UserDefinedType = 36,
+        Array = 8192,
+    }
+
+}
