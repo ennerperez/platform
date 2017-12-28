48d335f36b6c3fcbe3da5e593d0b1bf5c76b93fd
Enner Pérez <ennerperez@gmail.com>
2017-12-27 23:13:22 -0400

    CefResolver
    AnyCPU

---
+++ src/Presentation.Reports/CefAssemblyResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace Platform.Presentation.Reports
+{
+    public static class CefAssemblyResolve
+    {
+        internal static bool resolved;
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Resolve()
+        {
+            if (resolved)
+                return;
+
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            var settings = new CefSettings();
+
+            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                   Environment.Is64BitProcess ? "x64" : "x86",
+                                                   "CefSharp.BrowserSubprocess.exe");
+
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+
+            resolved = true;
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                       Environment.Is64BitProcess ? "x64" : "x86",
+                                                       assemblyName);
+
+                return File.Exists(archSpecificPath)
+                           ? Assembly.LoadFile(archSpecificPath)
+                           : null;
+            }
+
+            return null;
+        }
+    }
+}
