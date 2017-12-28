da232a1817fece62549ae948186d59f66c76d379
Enner Pérez <ennerperez@gmail.com>
2017-12-28 15:33:11 -0400

    Reports Id Fixed

--- src/Presentation.Reports/Properties/AssemblyInfo.cs
+++ src/Presentation.Reports/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Platform Presentation Reporting")]
+[assembly: AssemblyTitle("Platform Presentation Reports")]
 [assembly: AssemblyDescription("Platform Presentation for Reporting")]
 [assembly: Guid("c56b9867-7dc4-45b2-b314-fa599480e7fc")]
