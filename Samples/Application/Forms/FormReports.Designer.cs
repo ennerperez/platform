1082767b69329ae08548f237deae431bbd193939
Enner Pérez <ennerperez@gmail.com>
2017-12-29 17:21:06 -0400

    Samples Updates

--- Samples/Application/Forms/FormReports.Designer.cs
+++ Samples/Application/Forms/FormReports.Designer.cs
@@ -30,31 +30,36 @@
         private void InitializeComponent()
         {
             this.menuStripMain = new System.Windows.Forms.MenuStrip();
-            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.RDCLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rdclReportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.razorReportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveFileDialogExport = new System.Windows.Forms.SaveFileDialog();
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.dataGridViewDataSource = new System.Windows.Forms.DataGridView();
+            this.panelZoom = new System.Windows.Forms.Panel();
+            this.trackBarZoom = new System.Windows.Forms.TrackBar();
             this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDataSource)).BeginInit();
+            this.panelZoom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStripMain
             // 
             this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.RDCLToolStripMenuItem,
-            this.reportsToolStripMenuItem});
+            this.rdclReportsToolStripMenuItem,
+            this.razorReportsToolStripMenuItem});
             this.menuStripMain.Location = new System.Drawing.Point(0, 0);
             this.menuStripMain.Name = "menuStripMain";
             this.menuStripMain.Size = new System.Drawing.Size(624, 24);
             this.menuStripMain.TabIndex = 0;
             this.menuStripMain.Text = "menuStrip1";
-            // 
-            // reportsToolStripMenuItem
-            // 
-            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
-            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.reportsToolStripMenuItem.Text = "Ra&zor";
             // 
             // fileToolStripMenuItem
             // 
@@ -64,29 +69,89 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "&File";
             // 
-            // RDCLToolStripMenuItem
-            // 
-            this.RDCLToolStripMenuItem.Name = "RDCLToolStripMenuItem";
-            this.RDCLToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.RDCLToolStripMenuItem.Text = "&RDCL";
-            // 
             // exportToolStripMenuItem
             // 
             this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
-            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
             this.exportToolStripMenuItem.Text = "&Export...";
             this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // rdclReportsToolStripMenuItem
+            // 
+            this.rdclReportsToolStripMenuItem.Name = "rdclReportsToolStripMenuItem";
+            this.rdclReportsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.rdclReportsToolStripMenuItem.Text = "&RDCL";
+            // 
+            // razorReportsToolStripMenuItem
+            // 
+            this.razorReportsToolStripMenuItem.Name = "razorReportsToolStripMenuItem";
+            this.razorReportsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.razorReportsToolStripMenuItem.Text = "Ra&zor";
             // 
             // saveFileDialogExport
             // 
             this.saveFileDialogExport.DefaultExt = "html";
             this.saveFileDialogExport.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialogExport_FileOk);
             // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 24);
+            this.splitContainerMain.Name = "splitContainerMain";
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.dataGridViewDataSource);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.panelZoom);
+            this.splitContainerMain.Size = new System.Drawing.Size(624, 417);
+            this.splitContainerMain.SplitterDistance = 208;
+            this.splitContainerMain.TabIndex = 1;
+            // 
+            // dataGridViewDataSource
+            // 
+            this.dataGridViewDataSource.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewDataSource.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDataSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewDataSource.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewDataSource.Name = "dataGridViewDataSource";
+            this.dataGridViewDataSource.RowHeadersVisible = false;
+            this.dataGridViewDataSource.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewDataSource.Size = new System.Drawing.Size(208, 417);
+            this.dataGridViewDataSource.TabIndex = 0;
+            // 
+            // panelZoom
+            // 
+            this.panelZoom.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelZoom.Controls.Add(this.trackBarZoom);
+            this.panelZoom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelZoom.Location = new System.Drawing.Point(0, 379);
+            this.panelZoom.Name = "panelZoom";
+            this.panelZoom.Padding = new System.Windows.Forms.Padding(6);
+            this.panelZoom.Size = new System.Drawing.Size(412, 38);
+            this.panelZoom.TabIndex = 0;
+            this.panelZoom.Visible = false;
+            // 
+            // trackBarZoom
+            // 
+            this.trackBarZoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBarZoom.Location = new System.Drawing.Point(6, 6);
+            this.trackBarZoom.Maximum = 100;
+            this.trackBarZoom.Name = "trackBarZoom";
+            this.trackBarZoom.Size = new System.Drawing.Size(400, 26);
+            this.trackBarZoom.TabIndex = 0;
+            this.trackBarZoom.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarZoom.Value = 100;
+            this.trackBarZoom.ValueChanged += new System.EventHandler(this.trackBarZoom_ValueChanged);
+            // 
             // FormReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.splitContainerMain);
             this.Controls.Add(this.menuStripMain);
             this.MainMenuStrip = this.menuStripMain;
             this.Name = "FormReports";
@@ -94,6 +159,14 @@
             this.Shown += new System.EventHandler(this.FormMain_Shown);
             this.menuStripMain.ResumeLayout(false);
             this.menuStripMain.PerformLayout();
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDataSource)).EndInit();
+            this.panelZoom.ResumeLayout(false);
+            this.panelZoom.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,11 +176,15 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStripMain;
-        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem razorReportsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem RDCLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rdclReportsToolStripMenuItem;
         private System.Windows.Forms.SaveFileDialog saveFileDialogExport;
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.DataGridView dataGridViewDataSource;
+        private System.Windows.Forms.Panel panelZoom;
+        private System.Windows.Forms.TrackBar trackBarZoom;
     }
 }
 
--- Samples/Application/Forms/FormReports.cs
+++ Samples/Application/Forms/FormReports.cs
@@ -1,5 +1,6 @@
 ﻿using Platform.Presentation.Reports.Windows.Forms;
 using Platform.Samples.Core.Models;
+using Platform.Support.Collections;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -19,26 +20,24 @@ namespace Platform.Samples.Forms
 
         public Control ReportViewer { get; internal set; }
 
+        public List<Person> Models { get; internal set; }
+
         private void FormMain_Shown(object sender, System.EventArgs e)
         {
             foreach (var item in System.IO.Directory.GetFiles("reports", "*.cshtml"))
             {
                 var child = new ToolStripMenuItem(System.IO.Path.GetFileNameWithoutExtension(item));
                 child.Click += RazorChild_Click;
-                reportsToolStripMenuItem.DropDownItems.Add(child);
+                razorReportsToolStripMenuItem.DropDownItems.Add(child);
             }
-        }
-
-        private void RazorChild_Click(object sender, System.EventArgs e)
-        {
-            if (Controls.Contains(ReportViewer)) Controls.Remove(ReportViewer);
-
-            var Report = new RazorReportViewer()
+            foreach (var item in System.IO.Directory.GetFiles("reports", "*.rdcl"))
             {
-                Dock = DockStyle.Fill,
-            };
+                var child = new ToolStripMenuItem(System.IO.Path.GetFileNameWithoutExtension(item));
+                child.Click += RDCLChild_Click;
+                rdclReportsToolStripMenuItem.DropDownItems.Add(child);
+            }
 
-            var source = new List<Person>()
+            Models = new List<Person>()
                 {
                     new Person(){ Name="Enner Pérez", Age =31},
                     new Person(){ Name="Richard Pérez", Age =30},
@@ -47,26 +46,70 @@ namespace Platform.Samples.Forms
                     new Person(){ Name="Suami Cepeda", Age =30},
                 };
 
-            if ((sender as ToolStripMenuItem).Text == "Person")
+            dataGridViewDataSource.DataSource = Models;
+        }
+
+        private void RazorChild_Click(object sender, System.EventArgs e)
+        {
+            if (splitContainerMain.Panel2.Controls.Contains(ReportViewer))
+                splitContainerMain.Panel2.Controls.Remove(ReportViewer);
+
+            ReportViewer = null;
+            panelZoom.Visible = true;
+            trackBarZoom.Value = 100;
+
+            var viewer = new RazorReportViewer()
             {
-                Report.Model = source[0];
-            }
+                Dock = DockStyle.Fill,
+            };
+
+            if ((sender as ToolStripMenuItem).Text == "Person" && dataGridViewDataSource.SelectedRows.Count > 0)
+                viewer.Model = Models[dataGridViewDataSource.SelectedRows[0].Index];
             else if ((sender as ToolStripMenuItem).Text == "Persons")
-            {
-                Report.Model = source;
-            }
+                viewer.Model = Models;
 
-            Report.Template = File.ReadAllText($"reports\\{(sender as ToolStripMenuItem).Text}.cshtml");
-            Report.Refresh();
+            viewer.Template = File.ReadAllText($"reports\\{(sender as ToolStripMenuItem).Text}.cshtml");
+            ReportViewer = viewer;
 
-            ReportViewer = Report;
-
-            Controls.Add(ReportViewer);
+            splitContainerMain.Panel2.Controls.Add(ReportViewer);
             ReportViewer.BringToFront();
+            (viewer as RazorReportViewer).Show();
         }
 
         private void RDCLChild_Click(object sender, System.EventArgs e)
         {
+            if (splitContainerMain.Panel2.Controls.Contains(ReportViewer))
+                splitContainerMain.Panel2.Controls.Remove(ReportViewer);
+
+            ReportViewer = null;
+            panelZoom.Visible = false;
+            trackBarZoom.Value = 100;
+
+            var viewer = new Microsoft.Reporting.WinForms.ReportViewer()
+            {
+                Dock = DockStyle.Fill,
+            };
+
+            var report = new Platform.Presentation.Reports.RDLC.ReportBuilder<Person>
+            {
+                AutoGenerateReport = true
+            };
+
+            var ds = new System.Data.DataSet("Person");
+            ds.Tables.Add(Models.ToDataTable());
+            report.DataSource = ds;
+
+            var xml = report.BuildReport();
+            var rpt = report.BuildReportAsStream();
+
+            viewer.LocalReport.LoadReportDefinition(rpt);
+            viewer.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("Person", Models));
+            viewer.RefreshReport();
+
+            ReportViewer = viewer;
+            splitContainerMain.Panel2.Controls.Add(ReportViewer);
+            ReportViewer.BringToFront();
+            ReportViewer.Refresh();
         }
 
         private void exportToolStripMenuItem_Click(object sender, EventArgs e)
@@ -77,6 +120,15 @@ namespace Platform.Samples.Forms
 
         private void saveFileDialogExport_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
+        }
+
+        private void trackBarZoom_ValueChanged(object sender, EventArgs e)
+        {
+            if (ReportViewer == null)
+                return;
+
+            if (ReportViewer.GetType() == typeof(RazorReportViewer))
+                (ReportViewer as RazorReportViewer).Zoom = trackBarZoom.Value;
         }
     }
 }
