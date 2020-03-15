d9b9e56b4b9ea9e11e5917ec7a804fb4d7311fd4
Enner Pérez <ennerperez@gmail.com>
2020-03-15 14:40:40 -0500

    Samples Normalization II

---
+++ demo/Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using Platform.Support.Reflection;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Platform Console Samples")]
+[assembly: AssemblyDescription("Platform Console Samples Application")]
+[assembly: Guid("bd0b53c6-7c65-4e17-ab2c-ad3641a891dd")]
+[assembly: AssemblyBuildDate("2017-12-28")]
